67afc7f7bc77224e0d36bb5579f8face7b6d327d
NosheenIqbal <buttnosheen874@gmail.com>
2024-08-26 12:57:04 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,48 @@
+
+//chapter4
+//VARIABLE; LEGAL AND ILLEGALS
+//Question#1
+//1 legal variables: 
+//var username;''
+//$myvar; 
+//-variable ;
+
+//question#2
+//illegal variables
+//var @myvar;
+//var #myvar;
+//var 12name;
+//var !explanation;
+//var vfgg=1223
+
+//question#3
+//a). $ 1424 and myvar;
+//b). $ _ myName and name123;
+//c). _myName and name123;
+//d). myvar
+//e). alert var;
+
+//chapter5
+//question#1
+//1.
+//var num = 3 + 5;
+//document.write("the sum of 3 and 5 is : " + num + <br>)
+//2.
+//var num = 3 - 5;
+//document.write("the subtraction of 3 and 5 is : " + num + <br>)
+//var num = 3 * 5;
+//document.write("the multiplication of 3 and 5 is : " + num + <br>)
+//var num = 3 / 5;
+//document.write("the division of 3 and 5 is : " + num + <br>)
+//var num = 3 % 5;
+//document.write("the modulus of 3 and 5 is : " + num + <br>)
+//3.
+//4.
+//var ticketPrice = 600;
+//var buyingPrice = ticketPrice * 5;
+//document.write("total cost to buy 5 tickets to a movie is : " + buyingPrice + "PKR");
+//5.
+//var obt_marks = 500;
+//var tt_marks = 700;
+//document.write;( "the student percentage is : " +obt_marks/tt_marks*100).tofixed( 2); + "%"
+//6
